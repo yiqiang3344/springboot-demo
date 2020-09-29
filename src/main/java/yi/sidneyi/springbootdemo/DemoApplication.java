d5e57f9a93fab0df6bcb023f9fdccdfdcb7752fe
junqiang.yi <junqiang.yi@xinfei.cn>
2020-09-29 16:45:24 +0800

    初始化

--- src/main/java/yi/sidneyi/springbootdemo/DemoApplication.java
+++ src/main/java/yi/sidneyi/springbootdemo/DemoApplication.java
@@ -1,12 +1,12 @@
 package yi.sidneyi.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationListener;
 
 import java.util.Set;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(DemoApplication.class);
