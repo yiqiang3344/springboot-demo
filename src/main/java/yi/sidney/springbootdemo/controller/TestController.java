0bae7caf023f51c78e35ad3ef6afd2e368b7cfc6
junqiang.yi <junqiang.yi@xinfei.cn>
2020-09-30 15:29:10 +0800

    redis操作

--- src/main/java/yi/sidney/springbootdemo/controller/TestController.java
+++ src/main/java/yi/sidney/springbootdemo/controller/TestController.java
@@ -2,10 +2,13 @@ package yi.sidney.springbootdemo.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import yi.sidney.springbootdemo.dto.Request;
 import yi.sidney.springbootdemo.dto.Response;
 import yi.sidney.springbootdemo.dto.TestRequest;
+import yi.sidney.springbootdemo.utils.redis.RedisUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
@@ -49,5 +52,36 @@ public class TestController {
         JSONObject data = new JSONObject();
         data.put("test", request.getTest());
         return new Response().initSuccessRes(data);
+    }
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @RequestMapping(value = "redis/set", method = RequestMethod.GET)
+    @ResponseBody
+    public Response redisSet(@Validated Request request) {
+        boolean ret = redisUtil.set("yjqtest", "yjq");
+        return new Response().initSuccessRes(ret);
+    }
+
+    @RequestMapping(value = "redis/get/{key}", method = RequestMethod.GET)
+    @ResponseBody
+    public Response redisSet(@PathVariable String key) {
+        Object ret = redisUtil.get(key);
+        return new Response().initSuccessRes(ret);
+    }
+
+    @RequestMapping(value = "redis/push/{key}/{value}", method = RequestMethod.GET)
+    @ResponseBody
+    public Response redisPush(@PathVariable String key, @PathVariable String value) {
+        boolean ret = redisUtil.lSet(key, value);
+        return new Response().initSuccessRes(ret);
+    }
+
+    @RequestMapping(value = "redis/pop/{key}", method = RequestMethod.GET)
+    @ResponseBody
+    public Response redisPop(@PathVariable String key) {
+        Object ret = redisUtil.lPop(key);
+        return new Response().initSuccessRes(ret);
     }
 }
